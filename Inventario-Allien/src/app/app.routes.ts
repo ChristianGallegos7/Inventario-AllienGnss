de2a56e5f07b3e828f5a17062b79d22b4cf410ce
Lancelot <lancelot@sunset.es>
2025-11-01 18:06:43 -0500

    boton>#

--- Inventario-Allien/src/app/app.routes.ts
+++ Inventario-Allien/src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
     {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
         path: '',
         redirectTo: 'login',
         pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     },
     {
         path: 'bodega',
